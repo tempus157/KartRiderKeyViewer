c5a417c1f1fc2a5b8146301835a860775efc454b
Park Changyong <tempus157@gmail.com>
2022-01-18 13:15:09 +0900

    Add NativeInput class

---
+++ Assets/Scripts/NativeInput.cs
@@ -0,0 +1,14 @@
+using System.Runtime.InteropServices;
+
+public static class NativeInput {
+    [DllImport("user32.dll")]
+    private static extern short GetAsyncKeyState(int key);
+
+    public static bool KeyDown(int key) {
+        return (GetAsyncKeyState(key) & 0x8000) != 0;
+    }
+
+    public static bool KeyUp(int key) {
+        return (GetAsyncKeyState(key) & 0x8000) == 0;
+    }
+}
